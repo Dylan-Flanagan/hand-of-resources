7a2d92c9cb0134f253c2faad57ad5624ccc01b9c
Dylan <dylflanagan@gmail.com>
2022-12-01 19:21:05 -0800

    Resource 1: POST/films creates a new film test passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,48 +18,56 @@ describe('films routes', () => {
           "director": "Paul Thomas Anderson",
           "genre": "Period Drama",
           "id": "1",
+          "title": "There Will Be Blood",
           "year": 2007,
         },
         Object {
           "director": "Stanley Kubrick",
           "genre": "Dystopian Crime",
           "id": "2",
+          "title": "A Clockwork Orange",
           "year": 1971,
         },
         Object {
           "director": "Ethan Coen",
           "genre": "Comedy",
           "id": "3",
+          "title": "Raising Arizona",
           "year": 1987,
         },
         Object {
           "director": "Paul Thomas Anderson",
           "genre": "Comedy",
           "id": "4",
+          "title": "Punch-Drunk Love",
           "year": 2002,
         },
         Object {
           "director": "Danny Boyle",
           "genre": "Comedy-Drama",
           "id": "5",
+          "title": "Trainspotting",
           "year": 1996,
         },
         Object {
           "director": "Who Cares",
           "genre": "Bad",
           "id": "6",
+          "title": "Moonfall",
           "year": 2022,
         },
         Object {
           "director": "Wes Anderson",
           "genre": "Comedy",
           "id": "7",
+          "title": "The Grand Budapest Hotel",
           "year": 2014,
         },
         Object {
           "director": "Park Chan-wook",
           "genre": "Neo-Noir Action Thriller",
           "id": "8",
+          "title": "Oldboy",
           "year": 2003,
         },
       ]
@@ -74,10 +82,27 @@ describe('films routes', () => {
         "director": "Paul Thomas Anderson",
         "genre": "Period Drama",
         "id": "1",
+        "title": "There Will Be Blood",
         "year": 2007,
       }
     `);
   });
+
+  it('POST /films should create a new film', async () => {
+    const newFilm = {
+      title: 'Inglorious Basterds',
+      director: 'Quentin Tarantino',
+      genre: 'Action Comedy',
+      year: 2009,
+    };
+    const resp = await await request(app).post('/films').send(newFilm);
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      ...newFilm,
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
