5084f0c87f8fbf0b16e4d130a40882ac2819b510
Dylan <dylflanagan@gmail.com>
2022-12-03 16:45:50 -0800

    Resource 2 - GET /albums should return a list of albums

---
+++ __tests__/albums.test.js
@@ -0,0 +1,76 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('albums routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('GET /albums should return a list of albums', async () => {
+    const resp = await request(app).get('/albums');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(8);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "artist": "Thinking Fellers Union Local 282",
+          "genre": "Art Rock",
+          "id": "1",
+          "title": "Strangers from the Universe",
+          "year": 1994,
+        },
+        Object {
+          "artist": "Helvetia",
+          "genre": "Rock",
+          "id": "2",
+          "title": "The Acrobats",
+          "year": 2007,
+        },
+        Object {
+          "artist": "Black Moth Super Rainbow",
+          "genre": "Psychedelic/Electronic",
+          "id": "3",
+          "title": "Start a People",
+          "year": 2004,
+        },
+        Object {
+          "artist": "Animal Collective",
+          "genre": "Experimental Pop",
+          "id": "4",
+          "title": "Merriweather Post Pavilion",
+          "year": 2009,
+        },
+        Object {
+          "artist": "Slint",
+          "genre": "Rock",
+          "id": "5",
+          "title": "Spiderland",
+          "year": 1991,
+        },
+        Object {
+          "artist": "My Bloody Valentine",
+          "genre": "Shoegaze",
+          "id": "6",
+          "title": "Isnt Anything",
+          "year": 1988,
+        },
+        Object {
+          "artist": "Modest Mouse",
+          "genre": "Rock",
+          "id": "7",
+          "title": "Building Nothing Out of Something",
+          "year": 2000,
+        },
+        Object {
+          "artist": "Aphex Twin",
+          "genre": "Electronic",
+          "id": "8",
+          "title": "Richard D. James Album",
+          "year": 1996,
+        },
+      ]
+    `);
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db
 DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS albums;
 
 CREATE TABLE films (
   id BIGINT GENERATED ALWAYS AS IDENTITY,
@@ -21,3 +22,23 @@ VALUES
 ('Moonfall', 'Who Cares', 'Bad', 2022),
 ('The Grand Budapest Hotel', 'Wes Anderson', 'Comedy', 2014),
 ('Oldboy', 'Park Chan-wook', 'Neo-Noir Action Thriller', 2003);
+
+CREATE TABLE albums (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR NOT NULL,
+  artist VARCHAR NOT NULL, 
+  genre VARCHAR NOT NULL,
+  year INT NOT NULL
+);
+
+INSERT INTO
+albums (title, artist, genre, year) 
+VALUES 
+('Strangers from the Universe', 'Thinking Fellers Union Local 282', 'Art Rock', 1994),
+('The Acrobats', 'Helvetia', 'Rock', 2007),
+('Start a People', 'Black Moth Super Rainbow', 'Psychedelic/Electronic', 2004),
+('Merriweather Post Pavilion', 'Animal Collective', 'Experimental Pop', 2009),
+('Spiderland', 'Slint', 'Rock', 1991),
+('Isnt Anything', 'My Bloody Valentine', 'Shoegaze', 1988),
+('Building Nothing Out of Something', 'Modest Mouse', 'Rock', 2000),
+('Richard D. James Album', 'Aphex Twin', 'Electronic', 1996);
